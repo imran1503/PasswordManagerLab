3b70386545b00d87aae4bff2c98e6d96174a1747
Imran Latif <imran.latif1503@gmail.com>
2026-09-24 15:55:13 -0400

    Implement AES-GCM vault encryption and decryption
    Vault dictionary → JSON → AES-GCM encryption → ciphertext → AES-GCM decryption → original JSON

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -1,24 +1,47 @@
-from argon2 import PasswordHasher
+from argon2.low_level import hash_secret_raw, Type
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+import os
+import json
 
-ph = PasswordHasher()
+vault = {
+    "github": {
+        "username": "test@example.com",
+        "password": "FakePassword123!",
+        "url": "https://github.com"
+    }
+}
+def derive_key(password, salt):
+    return hash_secret_raw(
+        password.encode(),
+        salt,
+        time_cost=3,
+        memory_cost=65536,
+        parallelism=4,
+        hash_len=32,
+        type=Type.ID
+    )
 
-hashed = ph.hash("test-password")
+#Encypting a message and password using AES-GCM with a derived key from Argon2
+message = json.dumps(vault).encode()
+password = "test-password"
+salt = os.urandom(16)
 
-print(hashed)
+key = derive_key(password, salt)
 
-class PasswordEntry:
-    def __init__(self, name, username, password, url):
-        self.name = name
-        self.username = username
-        self.password = password
-        self.url = url
+aes = AESGCM(key)
 
-entry = PasswordEntry(
-    "GitHub",
-    "test@example.com",
-    "FakePassword123!",
-    "https://github.com"
-)
+# Generate a unique 12-byte nonce for this AES-GCM encryption. The nonce ensures that encrypting the same plaintext with the same key doesn't produce the same ciphertext every time.
+nonce = os.urandom(12)
 
-print(entry.name)
-print(entry.username)
+
+# Encrypt the vault data
+
+encrypted = aes.encrypt(nonce, message, None)
+
+print(encrypted)
+
+#Decrypting the message using the same derived key and nonce. 
+
+decrypted = aes.decrypt(nonce, encrypted, None)
+
+print(decrypted)
